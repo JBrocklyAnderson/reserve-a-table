6f5020487c0f1deb1770d02643bf9c713be6fe5d
JBrocklyAnderson <140027721+JBrocklyAnderson@users.noreply.github.com>
2024-04-12 11:48:14 -0700

    Fix the responsive layout of navigation bar

--- src/components/common/Nav.jsx
+++ src/components/common/Nav.jsx
@@ -9,38 +9,48 @@ export const placeholderLinkAlert = (e) => {
 };
 
 const Nav = () => {
-    const [toggle, setToggle] = useState(false);
+    {/* Initialize state to track menu toggle */}
+    const [isOpen, setIsOpen] = useState(false);
 
     const toggleNavbar = () => {
-        setToggle(!toggle);
+        setIsOpen(isOpen => !isOpen);
     }
 
     return (
-        <nav>
-            <Link to='/'>
-                <img src={Logo} alt='Little Lemon Logo' />
-            </Link>
+        <nav className={styles.navBar}>
+            <div className={styles.mobile}>
+                <Link to='/'>
+                    <img src={Logo} alt='Little Lemon Logo' />
+                </Link>
 
-            <svg className={`${styles.menu}`} xmlns='http://www.w3.org/2000/svg' viewBox='0 0 448 512' width={40} onClick={toggleNavbar}>
-                {/* <!-- !Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--> */}
-                <path fill='var(--olive)' d='M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z' />
-            </svg>
+                {!isOpen ? (
+                    <svg className={`${styles.menu}`} xmlns='http://www.w3.org/2000/svg' viewBox='0 0 448 512' onClick={toggleNavbar}>
+                        {/* <!-- !Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--> */}
+                        <path d='M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z' />
+                    </svg>
+                ) : (
+                    <svg className={`${styles.menu}`} xmlns='http://www.w3.org/2000/svg' viewBox='0 0 384 512' onClick={toggleNavbar}>
+                        {/* <!-- !Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc. -->*/}
+                        <path d='M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z'/>
+                    </svg>
+                )}
+            </div>
 
-            <ul className={`${styles.navbar} ${toggle ? styles.flex : ''}`}>
-                <li className={styles.navbarItem}>
-                    <Link to='/'>Home</Link>
+            <ul className={`${styles.links} ${isOpen ? styles.open : ''}`}>
+                <li>
+                    <Link to='/' className={`${styles.clickable} ${styles.link}`}>Home</Link>
                 </li>
-                <li className={styles.navbarItem}>
-                    <Link to='/our-story'>About</Link>
+                <li>
+                    <Link to='/our-story' className={`${styles.clickable} ${styles.link}`}>About</Link>
                 </li>
-                <li className={styles.navbarItem}>
-                    <Link to='/menu'>Menu</Link>
+                <li>
+                    <Link to='/menu' className={`${styles.clickable} ${styles.link}`}>Menu</Link>
                 </li>
-                <li className={styles.navbarItem}>
-                    <Link to='/reserve-a-table'>Reservations</Link>
+                <li>
+                    <Link to='/reserve-a-table' className={`${styles.clickable} ${styles.link}`}>Reservations</Link>
                 </li>
                 <li className={styles.navbarBtn}>
-                    <Link to='/order-online' onClick={placeholderLinkAlert}>Order Online</Link>
+                    <Link to='/order-online' className={`${styles.clickable} ${styles.link}`} onClick={placeholderLinkAlert}>Order Online</Link>
                 </li>
             </ul>
         </nav>
