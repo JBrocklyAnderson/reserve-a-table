32b1bc2da9f1136bcf12bfe71e1e4efca30d7e1d
JBrocklyAnderson <140027721+JBrocklyAnderson@users.noreply.github.com>
2024-04-30 20:49:52 -0700

    Reimplement scroll-to-top functionality

--- src/App.js
+++ src/App.js
@@ -2,15 +2,8 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import routes from './routes';
 import Layout from './components/Layout';
-// import Scroll from './components/context/Scroll';
-// import Nav from './components/common/Nav';
-import Home from './components/home/Home';
-import About from './components/about/About';
-import ReservationForm from './components/reservationForm/ReservationForm';
-import ReservationConfirmed from './components/reservationConfirmed/ReservationConfirmed';
-// import Footer from './components/common/Footer';
+import Scroll from './components/context/Scroll';
 import './App.css';
-
 
 
 const App = () => {
@@ -23,21 +16,6 @@ const App = () => {
 
   return (
     <RouterProvider router={router} />
-
-    /* <BrowserRouter>
-      <Nav />
-
-      <Scroll />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/our-story' element={<About />} />
-        {/* <Route path='/menu' element={<Menu />} />
-        <Route path='/reserve-a-table' element={<ReservationForm />} />
-        <Route path='/reservation-confirmed' element={<ReservationConfirmed />} />
-      </Routes>
-
-      <Footer />
-  </BrowserRouter> */
   );
 };
 
